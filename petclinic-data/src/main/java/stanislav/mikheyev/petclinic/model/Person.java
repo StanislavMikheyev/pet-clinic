4abaced7bdb472576c5a222d9e3e8dbc9ccbabb4
Stanislav Mikheyev <stanislav.mikheyev@gmail.com>
2019-05-03 18:06:56 +0200

    Introduce BaseEntity

--- petclinic-data/src/main/java/stanislav/mikheyev/petclinic/model/Person.java
+++ petclinic-data/src/main/java/stanislav/mikheyev/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package stanislav.mikheyev.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- petclinic-data/src/main/java/stanislav/mikheyev/petclinic/model/PetType.java
+++ petclinic-data/src/main/java/stanislav/mikheyev/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package stanislav.mikheyev.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
